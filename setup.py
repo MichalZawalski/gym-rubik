76ee122190023209e7a8e545d13978c9c3339451
Michał Zawalski <michal.zawalski25@gmail.com>
2023-11-14 15:10:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_rubik',
       version='0.0.1',
+      packages=find_packages(),
+      include_package_data=True,
       install_requires=['gym', 'matplotlib', 'numpy']  # And any other dependencies rubik needs
-      )
+)
